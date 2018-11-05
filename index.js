a5bcc9a7576028853038b835b060a23ff31a5353
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-05 14:32:44 -0500

    mode database functions to model file

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-const pgp = require('pg-promise')();
-const db = pgp({
-	host: 'localhost',
-	port: 5432,
-	database: 'node-todo-app-db'
-});
-
-// db.any('select * from todos').then((results) => {
-// 	console.log(results);
-// });
-// getAll()
-//     .then(results => {
-//         console.log(results);
-//         console.log(`yep, those were all the todos`)
-//     })
-
-function getById(id) {
-	return db.one(`select * from todos where id = $1`, [ id ]).catch((err) => {
-		return {
-			name: 'No todo found'
-		};
-	});
-}
-
 // getById(2).then((result) => {
 // 	console.log(result);
 // });
-
-// example of adding a row
-function add(name, completed) {
-	return db.result(
-		`insert into todos (name,completed)
-        values
-        ($1, $2)
-        returning id
-        `,
-		[ name, completed ]
-	);
-}
 
 // add('walk the chewbacca', false)
 // 	.catch((err) => {
@@ -46,12 +10,10 @@ function add(name, completed) {
 // 		console.log(result);
 // 	});
 
-// example of deleting a row
-function deleteById(id) {
-	return db.result(`delete from todos where id = $1`, [ id ]);
-}
+markPending(1).then((result) => {
+	console.log(result);
+});
+
 // deleteById(6).then((result) => {
 // 	console.log(result);
 // });
-
-// example of updating a row
