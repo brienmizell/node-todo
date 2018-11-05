08600faf59d8eabb90cda203a5e0e843802501af
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-05 14:35:35 -0500

    mode database functions to model file

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-// getById(2).then((result) => {
-// 	console.log(result);
-// });
+// getAll()
+//     .then(results => {
+//         console.log(results);
+//         console.log(`yep those were the todos. cool.`)
+//     })
+
+// getById(2)
+//     .then(result => { console.log(result); })
+
+// getById(2000000)
+//     .then(result => { console.log(result); })
 
 // add('walk the chewbacca', false)
-// 	.catch((err) => {
-// 		console.log(err);
-// 	})
-// 	.then((result) => {
-// 		console.log(result);
-// 	});
+//     .catch(err => {
+//         console.log(err);
+//     })
+//     .then(result => {
+//         console.log(result);
+//     })
 
-markPending(1).then((result) => {
-	console.log(result);
-});
+// updateName(2, 'buy new hyperdrive')
+//     .then(result => {
+//         console.log(result);
+//     })
 
-// deleteById(6).then((result) => {
-// 	console.log(result);
-// });
+// markPending(1)
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// deleteById(10)
+//     .then(result => {
+//         console.log(result.rowCount);
+//     })
