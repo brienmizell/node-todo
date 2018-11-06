b53711be38e7b0174cc4a8947e018c44c36a32f3
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-06 14:00:37 -0500

    adds searchByName

--- index.js
+++ index.js
@@ -1,19 +1,82 @@
 require('dotenv').config();
 
-const Todo = require('./models/Todo');
+// const Todo = require('./models/Todo');
 const User = require('./models/User');
 
-User.deleteById(2).then((result) => {
-	console.log(result);
+User.searchByName('jef').then((users) => {
+	console.log(users);
 });
+
+// User.getById(6)
+//     .then(u => {
+//         u.delete();
+//     });
+
+// User.deleteById(8);
+
+// User.getAll()
+//     .then(allUsers => {
+//         allUsers.forEach(user => {
+//             console.log(user.name);
+//         });
+//     })
+
+// User.getById(1)
+//     .then(userFromDB => {
+//         console.log(userFromDB);
+//         userFromDB.getTodos()
+//             .then(todos => {
+//                 console.log(todos);
+//             })
+//     });
+
+// const beth = new User(2, 'beth');
+// beth.getTodos()
+//     .then(result => { console.log(result); })
+
+// let newUsers = [
+//     'jeff',
+//     'brandy',
+//     'zack',
+//     'tasha',
+//     'jenn',
+//     'cori'
+// ];
+
+// newUsers.forEach(u => {
+//     User.add(u)
+//         .then(aNewUser => {
+//             aNewUser.addTodo('do the thing');
+//         })
+// });
+
+// User.add('jeff')
+// User.add('jeff')
+// User.add('jeff')
+//     .then(theNewUser => {
+//         theNewUser.getTodos()
+//             .then(todos => {
+//                 console.log(`${theNewUser.name} has ${todos.length} things todo`);
+//             })
+//     })
+
+// const skyler = new User('Skyler the Dog');
+// const ahjuma = new User('Ahjuma the Impressive');
+
+// // debugger;
+
+// skyler.greet(ahjuma);
+// ahjuma.greet(skyler);
 
 // let u = User.findById(1);
-// u.name = 'eileeeeeen';
+// u.name = 'eileeeeeeen';
 // u.save();
 
-Todo.deleteById('asdfasdfasf').then((result) => {
-	console.log(result);
-});
+// User.deleteById('asdfasdfasf')
+//     .then(result => { console.log(result); })
+
+// Todo.deleteById(1)
+//     .then(result => { console.log(result); })
 
 // User.getTodosForUser(3)
 //     .then(result => { console.log(result); })
--- models/Users.js
+++ models/Users.js
@@ -7,66 +7,147 @@ class User {
 	// `constructor` is a method
 	// that is automatically
 	// called when you create a user
-	constructor() {
+	constructor(id, name) {
 		// define properties that
 		// are also the names
-		// of the database columns.
+		// of the database columns
+		this.id = id;
 		this.name = name;
 	}
 
-	// a method is a function "belongs"
-	// to an object
+	// CREATE
+	static add(name) {
+		return db
+			.one(
+				`
+            insert into users 
+                (name)
+            values
+                ($1)
+            returning id    
+            `,
+				[ name ]
+			)
+			.then((data) => {
+				const u = new User(data.id, name);
+				return u;
+			});
+	}
+
+	// RETRIEVE
+	static getAll() {
+		return db
+			.any(
+				`
+            select * from users
+        `
+			)
+			.then((userArray) => {
+				// transform array of objects
+				// into array of User instances
+				const instanceArray = userArray.map((userObj) => {
+					const u = new User(userObj.id, userObj.name);
+					return u;
+				});
+				return instanceArray;
+			});
+	}
+
+	static getById(id) {
+		return db.one('select * from users where id = $1', [ id ]).then((result) => {
+			const u = new User(result.id, result.name);
+			return u;
+		});
+	}
+
+	static searchByName(name) {
+		return db.any(
+			`
+            select * from users
+                where name ilike '%$1:raw%'
+        `,
+			[ name ]
+		);
+	}
+
+	getTodos() {
+		return db.any(
+			`
+            select * from todos
+                where user_id = $1
+        `,
+			[ this.id ]
+		);
+	}
+
+	// UPDATE
+
+	// DELETE
+	delete() {
+		return db.result(
+			`
+        delete from users
+        where id = $1
+        `,
+			[ this.id ]
+		);
+	}
+
+	static deleteById(id) {
+		return db.result(
+			`
+        delete from users
+        where id = $1
+        `,
+			[ id ]
+		);
+	}
+
+	// // a method is a function "belongs"
+	// // to an object
 	greet(otherUser) {
-		console.log(`hello ${otherUser.name}, I am ${this.name}`);
+		console.log(`Hello ${otherUser.name}, I am ${this.name}`);
 	}
 }
 
-function doThing() {}
-
 // ============================================
 // CREATE
-function add(name) {
-	return db.one(
-		`
-        insert into users 
-            (name)
-        values
-            ($1)
-        returning id    
-    `,
-		[ name ]
-	);
-}
+// function add(name) {
+//     return db.one(`
+//         insert into users
+//             (name)
+//         values
+//             ($1)
+//         returning id
+//     `, [name])
+// }
 
-// ============================================
+// // ============================================
 // RETRIEVE
-function getAll() {
-	return db.any(`
-        select  
-            users.id,
-            users.name,
-            t.name as todo,
-            t.completed as completed
-        from 
-            users
-            left join todos t
-            on users.id = t.user_id
-    `);
-}
+// function getAll() {
+//     return db.any(`
+//         select
+//             users.id,
+//             users.name,
+//             t.name as todo,
+//             t.completed as completed
+//         from
+//             users
+//             left join todos t
+//             on users.id = t.user_id
+//     `);
+// }
 
-function getById(id) {
-	return db.one('select * from users where id = $1', [ id ]);
-}
+// function getById(id) {
+//     return db.one('select * from users where id = $1', [id]);
+// }
 
-function getTodosForUser(id) {
-	return db.any(
-		`
-        select * from todos
-            where user_id = $1
-    `,
-		[ id ]
-	);
-}
+// function getTodosForUser(id) {
+//     return db.any(`
+//         select * from todos
+//             where user_id = $1
+//     `, [id]);
+// }
 
 // ============================================
 // UPDATE
@@ -83,24 +164,22 @@ function updateName(id, name) {
 
 // ============================================
 // DELETE
-function deleteById(id) {
-	return db.result(
-		`
-    delete from users
-    where id = $1
-    `,
-		[ id ]
-	);
-}
+// function deleteById(id) {
+//     return db.result(`
+//     delete from users
+//     where id = $1
+//     `, [id]);
+// }
 
 // ============================================
 
-module.exports = {
-	add,
-	deleteById,
-	getAll,
-	getById,
-	getTodosForUser,
-	updateName,
-	User
-};
+module.exports = User;
+// module.exports = {
+//     add,
+//     deleteById,
+//     getAll,
+//     getById,
+//     getTodosForUser,
+//     updateName,
+//     User
+// };
