e4b9f7063620d261c32c5c98af1e0451cc547783
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-09 21:10:16 -0500

    added html

--- index.js
+++ index.js
@@ -17,37 +17,40 @@ const User = require('./models/User');
 
 const page = require('./views/page');
 const userList = require('./views/userList');
+const todoList = require('./views/todoList');
 
 app.get('/', (req, res) => {
-	const thePage = page('hey there');
-	res.send(thePage);
+    const thePage = page('hey there');
+    res.send(thePage);
 });
 
 // Listen for a GET request
 app.get('/users', (req, res) => {
-	User.getAll().then((allUsers) => {
-		// res.status(200).json(allUsers);
-		// res.send(allUsers);
-		const usersUL = userList(allUsers);
-		const thePage = page(usersUL);
-		console.log(thePage);
-		res.send(thePage);
+    User.getAll()
+        .then(allUsers => {
+            // res.status(200).json(allUsers);
+            // res.send(allUsers);
+            const usersUL = userList(allUsers);
+            const thePage = page(usersUL);
+            console.log(thePage);
+            res.send(thePage);
 
-		// res.send(page(userList(allUsers)));
-	});
+            // res.send(page(userList(allUsers)));
+        });
 });
 
 // Listen for POST requests
 // Create a new user
 app.post('/users', (req, res) => {
-	console.log(req);
-	// console.log(req.body);
-	// res.send('ok');
-	const newUsername = req.body.name;
-	console.log(newUsername);
-	User.add(newUsername).then((theUser) => {
-		res.send(theUser);
-	});
+    console.log(req);
+    // console.log(req.body);
+    // res.send('ok');
+    const newUsername = req.body.name;
+    console.log(newUsername);
+    User.add(newUsername)
+        .then(theUser => {
+            res.send(theUser);
+        })
 });
 
 // Updating an existing user
@@ -56,23 +59,26 @@ app.post('/users', (req, res) => {
 // app.post('/users/:id(\\d+)', (req, res) => {
 // app.post(/^\/users\/:id(\d+)/, (req, res) => {
 app.post('/users/:id([0-9]+)', (req, res) => {
-	const id = req.params.id;
-	const newName = req.body.name;
-	console.log(id);
-	console.log(newName);
-	// res.send('ok');
+    const id = req.params.id;
+    const newName = req.body.name;
+    console.log(id);
+    console.log(newName);
+    // res.send('ok');
 
-	// Get the user by their id
-	User.getById(id).then((theUser) => {
-		// call that user's updateName method
-		theUser.updateName(newName).then((result) => {
-			if (result.rowCount === 1) {
-				res.send('yeah you did');
-			} else {
-				res.send('💩');
-			}
-		});
-	});
+    // Get the user by their id
+    User.getById(id)
+        .then(theUser => {
+            // call that user's updateName method
+            theUser.updateName(newName)
+                .then(result => {
+                    if (result.rowCount === 1) {
+                        res.send('yeah you did');
+                    } else {
+                        res.send('💩');
+                    }
+                });
+            
+        });
 });
 
 // Example of grabbing a user by an
@@ -96,41 +102,66 @@ app.post('/users/:id([0-9]+)', (req, res) => {
 //                         res.send('💩');
 //                     }
 //                 });
-
+            
 //         });
 // });
+
+
+
+
+
+
+
+
+
+
 
 // Match the string "/users/" followed by one or more digits
 // REGular EXpressions
 // app.get('/users/:id([0-9]+)', (req, res) => {
 app.get(`/users/:id(\\d+)`, (req, res) => {
-	// console.log(req.params.id);
-	User.getById(req.params.id)
-		.catch((err) => {
-			res.send({
-				message: `no soup for you`
-			});
-		})
-		.then((theUser) => {
-			res.send(theUser);
-		});
+    // console.log(req.params.id);
+    User.getById(req.params.id)
+        .catch(err => {
+            res.send({
+                message: `no soup for you`
+            });
+        })
+        .then(theUser => {
+            res.send(theUser);
+        })
+});
+
+app.get(`/users/:id(\\d+)/todos`, (req, res) => {
+    User.getById(req.params.id)
+        .then(theUser => {
+            theUser.getTodos()
+                .then(allTodos => {
+                    const todosUL = todoList(allTodos);
+                    const thePage = page(todosUL);
+                    res.send(thePage);
+                })
+        })
 });
 
 app.get('/users/register', (req, res) => {
-	res.send('you are on the registration page. no really.');
+    res.send('you are on the registration page. no really.');
 });
 
 app.get('/users/:id(\\d+)/rename/:newName', (req, res) => {
-	User.getById(req.params.id).then((user) => {
-		user.updateName(req.params.newName).then(() => {
-			res.send('you just renamed them!');
-		});
-	});
+    User.getById(req.params.id)
+        .then(user => {
+            user.updateName(req.params.newName)
+                .then(() => {
+                    res.send('you just renamed them!');
+                })
+        })
 });
 
 app.listen(3000, () => {
-	console.log('You express app is ready!');
+    console.log('You express app is ready!');
 });
+
 
 // ===== example of sending a whole page
 
--- models/Todo.js
+++ models/Todo.js
@@ -1,131 +1,84 @@
 const db = require('./db');
 
-class Todo {
-	// what properties should
-	// a user start off with?
-	// `constructor` is a method
-	// that is automatically
-	// called when you create a user
-	constructor(id, name, completed) {
-		// define properties that
-		// are also the names
-		// of the database columns
-		this.id = id;
-		this.name = name;
-		this.completed = completed;
-	}
-	/////////////////////
-	// create
-	/////////////////////
-	static add(name, completed) {
-		return db
-			.one(
-				`
-		insert into todos 
-			(name, completed) 
-		values 
-			($1, $2) returning id`,
-				[ name, completed ]
-			)
-			.then((data) => {
-				const u = new User(data.id, name, completed);
-				return u;
-			});
-	}
 
-	/////////////////////
-	// retrieve
-	/////////////////////
-
-	static getAll() {
-		return db.any('select * from todos');
-		console.log();
-	}
+// CREATE
+function add(name, completed) {
+    return db.one(`insert into todos (name, completed)
+        values
+            ($1, $2)
+        returning id    
+    `, [name, completed])
 }
 
-///////////////////////////////////
-// create
-///////////////////////////////////
+// RETRIEVE
+// example of grabbing all the rows
+function getAll() {
+    return db.any('select * from todos');
+}
 
-// // CREATE
-// function add(name, completed) {
-//     return db.one(`insert into todos (name, completed)
-//         values
-//             ($1, $2)
-//         returning id
-//     `, [name, completed])
-// }
+// example of grabbing one row
+function getById(id){
+    return db.one(`select * from todos where id = $1`, [id])
+        .catch(err => {
+            // Got nuthin'
+            // console.log('you did not get a todo');
+            return {
+                name: 'No todo found.'
+            };   
+        })
+}
 
-// // RETRIEVE
-// // example of grabbing all the rows
-// function getAll() {
-//     return db.any('select * from todos');
-// }
+// UPDATE
 
-// // example of grabbing one row
-// function getById(id){
-//     return db.one(`select * from todos where id = $1`, [id])
-//         .catch(err => {
-//             // Got nuthin'
-//             // console.log('you did not get a todo');
-//             return {
-//                 name: 'No todo found.'
-//             };
-//         })
-// }
+function assignToUser(todoId, userId) {
+    return db.result(`
+        update todos
+            set user_id = $2
+        where id = $1    
+    `, [todoId, userId]);
+}
 
-// // UPDATE
+// example of updating a row
+function updateName(id, name) {
+    return db.result(`update todos
+        set name=$2
+    where id=$1`, [id, name]);
+}
 
-// function assignToUser(todoId, userId) {
-//     return db.result(`
-//         update todos
-//             set user_id = $2
-//         where id = $1
-//     `, [todoId, userId]);
-// }
 
-// // example of updating a row
-// function updateName(id, name) {
-//     return db.result(`update todos
-//         set name=$2
-//     where id=$1`, [id, name]);
-// }
+function updateCompleted(id, didComplete) {
+    return db.result(`update todos 
+        set completed=$2 
+    where id=$1`, [id, didComplete])    
+}
 
-// function updateCompleted(id, didComplete) {
-//     return db.result(`update todos
-//         set completed=$2
-//     where id=$1`, [id, didComplete])
-// }
+function markCompleted(id) {
+    // return updateCompleted(id, true);
+    return db.result(`update todos 
+	                    set completed=$2 
+	                where id=$1`, [id, true]);
+}
 
-// function markCompleted(id) {
-//     // return updateCompleted(id, true);
-//     return db.result(`update todos
-// 	                    set completed=$2
-// 	                where id=$1`, [id, true]);
-// }
+function markPending(id) {
+    // return updateCompleted(id, false);
+    return db.result(`update todos 
+	                    set completed=$2 
+	                where id=$1`, [id, false]);
+}
 
-// function markPending(id) {
-//     // return updateCompleted(id, false);
-//     return db.result(`update todos
-// 	                    set completed=$2
-// 	                where id=$1`, [id, false]);
-// }
+// DELETE
+// example of deleting a row
+function deleteById(id){
+    return db.result(`delete from todos where id = $1`, [id])
+}
 
-// // DELETE
-// // example of deleting a row
-// function deleteById(id){
-//     return db.result(`delete from todos where id = $1`, [id])
-// }
-
-// module.exports = {
-//     add,
-//     assignToUser,
-//     deleteById,
-//     getAll,
-//     getById,
-//     markCompleted,
-//     markPending,
-//     updateName,
-// };
-
-module.exports = Todo;
+module.exports = {
+    add,
+    assignToUser,
+    deleteById,
+    getAll,
+    getById,
+    markCompleted,
+    markPending,
+    updateName,
+};
