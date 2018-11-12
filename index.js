720969579a0617aa4921a873b1fae2aa2c2a40ed
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-12 09:30:10 -0500

    adds example of further differentiating your routes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(express.static('public'));
-
 // Configure body-parser to read data sent by HTML form tags
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,42 +13,25 @@ app.use(bodyParser.json());
 // const Todo = require('./models/Todo');
 const User = require('./models/User');
 
-const page = require('./views/page');
-const userList = require('./views/userList');
-const todoList = require('./views/todoList');
-
-app.get('/', (req, res) => {
-    const thePage = page('hey there');
-    res.send(thePage);
-});
-
 // Listen for a GET request
 app.get('/users', (req, res) => {
-    User.getAll()
-        .then(allUsers => {
-            // res.status(200).json(allUsers);
-            // res.send(allUsers);
-            const usersUL = userList(allUsers);
-            const thePage = page(usersUL);
-            console.log(thePage);
-            res.send(thePage);
-
-            // res.send(page(userList(allUsers)));
-        });
+	User.getAll().then((allUsers) => {
+		// res.status(200).json(allUsers);
+		res.send(allUsers);
+	});
 });
 
 // Listen for POST requests
 // Create a new user
 app.post('/users', (req, res) => {
-    console.log(req);
-    // console.log(req.body);
-    // res.send('ok');
-    const newUsername = req.body.name;
-    console.log(newUsername);
-    User.add(newUsername)
-        .then(theUser => {
-            res.send(theUser);
-        })
+	console.log(req);
+	// console.log(req.body);
+	// res.send('ok');
+	const newUsername = req.body.name;
+	console.log(newUsername);
+	User.add(newUsername).then((theUser) => {
+		res.send(theUser);
+	});
 });
 
 // Updating an existing user
@@ -59,26 +40,23 @@ app.post('/users', (req, res) => {
 // app.post('/users/:id(\\d+)', (req, res) => {
 // app.post(/^\/users\/:id(\d+)/, (req, res) => {
 app.post('/users/:id([0-9]+)', (req, res) => {
-    const id = req.params.id;
-    const newName = req.body.name;
-    console.log(id);
-    console.log(newName);
-    // res.send('ok');
+	const id = req.params.id;
+	const newName = req.body.name;
+	console.log(id);
+	console.log(newName);
+	// res.send('ok');
 
-    // Get the user by their id
-    User.getById(id)
-        .then(theUser => {
-            // call that user's updateName method
-            theUser.updateName(newName)
-                .then(result => {
-                    if (result.rowCount === 1) {
-                        res.send('yeah you did');
-                    } else {
-                        res.send('💩');
-                    }
-                });
-            
-        });
+	// Get the user by their id
+	User.getByName(id).then((theUser) => {
+		// call that user's updateName method
+		theUser.updateName(newName).then((result) => {
+			if (result.rowCount === 1) {
+				res.send('yeah you did');
+			} else {
+				res.send('💩');
+			}
+		});
+	});
 });
 
 // Example of grabbing a user by an
@@ -102,66 +80,41 @@ app.post('/users/:id([0-9]+)', (req, res) => {
 //                         res.send('💩');
 //                     }
 //                 });
-            
+
 //         });
 // });
-
-
-
-
-
-
-
-
-
-
 
 // Match the string "/users/" followed by one or more digits
 // REGular EXpressions
 // app.get('/users/:id([0-9]+)', (req, res) => {
 app.get(`/users/:id(\\d+)`, (req, res) => {
-    // console.log(req.params.id);
-    User.getById(req.params.id)
-        .catch(err => {
-            res.send({
-                message: `no soup for you`
-            });
-        })
-        .then(theUser => {
-            res.send(theUser);
-        })
-});
-
-app.get(`/users/:id(\\d+)/todos`, (req, res) => {
-    User.getById(req.params.id)
-        .then(theUser => {
-            theUser.getTodos()
-                .then(allTodos => {
-                    const todosUL = todoList(allTodos);
-                    const thePage = page(todosUL);
-                    res.send(thePage);
-                })
-        })
+	// console.log(req.params.id);
+	User.getById(req.params.id)
+		.catch((err) => {
+			res.send({
+				message: `no soup for you`
+			});
+		})
+		.then((theUser) => {
+			res.send(theUser);
+		});
 });
 
 app.get('/users/register', (req, res) => {
-    res.send('you are on the registration page. no really.');
+	res.send('you are on the registration page. no really.');
 });
 
 app.get('/users/:id(\\d+)/rename/:newName', (req, res) => {
-    User.getById(req.params.id)
-        .then(user => {
-            user.updateName(req.params.newName)
-                .then(() => {
-                    res.send('you just renamed them!');
-                })
-        })
+	User.getById(req.params.id).then((user) => {
+		user.updateName(req.params.newName).then(() => {
+			res.send('you just renamed them!');
+		});
+	});
 });
 
 app.listen(3000, () => {
-    console.log('You express app is ready!');
+	console.log('You express app is ready!');
 });
-
 
 // ===== example of sending a whole page
 
