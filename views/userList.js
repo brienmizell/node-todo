a6647bb8145ca435f484f9b180b655ebf4d35159
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-12 11:55:03 -0500

    added bcrypt

--- views/userList.js
+++ views/userList.js
@@ -1,17 +1,16 @@
-
 function userToItem(userObject) {
-    return `
+	return `
         <li class="user-list-item">
           <a href="/users/${userObject.id}/todos">
             ${userObject.name}
           </a>
-        </li>
+        <a href="/users/${userObject.id}>(edit)</a>
     `;
 }
 function userList(arrayOfUsers) {
-    const userItems = arrayOfUsers.map(userToItem).join('');
-    console.log(userItems);
-    return `
+	const userItems = arrayOfUsers.map(userToItem).join('');
+	console.log(userItems);
+	return `
         <ul>${userItems}</ul>
     `;
 }
